fbff687dc5ef43fb1a7adcf2fdd82953d1bc4037
Nitin <nitinkm320@gmail.com>
2023-10-08 10:22:03 +0530

    Starting the app

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import './main.css'
+import {AiOutlinePlus} from 'react-icons/ai'
+import Todo from '@/todo'
+
+export default function Home() {
+
+  const [todos, setTodos] = useState(['Do the Dishes', 'Eat Food'])
+
+  return (
+    <main>
+
+      <div className="todo-main">
+        <div className="todo-header">Todo-App</div>
+        <div className="todo-addition">
+          <input type="text" placeholder='Enter your todo...'/>
+          <button><AiOutlinePlus/></button>
+        </div>
+      </div>
+
+      <Todo/>
+      
+    </main>
+  )
+}
