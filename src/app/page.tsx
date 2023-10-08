daeb66926d48a205b104382a06103913dc739584
Nitin <nitinkm320@gmail.com>
2023-10-08 11:34:30 +0530

    Starting the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react'
 import './main.css'
 import {AiOutlinePlus} from 'react-icons/ai'
-import Todo from '@/todo'
+import Todo from './todo'
 
 export default function Home() {
 
