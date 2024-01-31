be7cd9d3e711187b8012e7cf87a780f9bead0cb1
Nitin <nitinkm320@gmail.com>
2024-01-31 22:30:25 +0530

    Added done

--- page.tsx
+++ page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import './index.css'
-import pic1 from './pics/pic1.jpg'
+import pic2 from "./pics/pic2.png"
 import { todo } from "node:test";
 import { QuerySnapshot, addDoc, collection, deleteDoc, doc, onSnapshot, query, updateDoc } from "firebase/firestore";
 import {db} from "./firebase.js";
@@ -51,7 +51,7 @@ export default function Home() {
     <main>
       <div className="main">
         <div className="left">
-          <Image src={pic1} alt="" className="left-main-img" height={200} width={300} />
+          <Image src={pic2} alt="" className="left-main-img" height={250} width={300} />
           <h1 className="head">Todo App</h1>
           <input value={input} onChange={(e) => setInput(e.target.value)} className="task" type="text" name="" id="" placeholder="What's on your mind ? "/>
           <button onClick={createTodo} className="submit">Submit</button>
@@ -72,7 +72,7 @@ function Todo({stuff, toggleComplete, deleteTodo}: {stuff: any, toggleComplete: 
     <div className="todo-main">
       <input onChange={() => toggleComplete(stuff)} type="checkbox" />
       <p>{stuff.text}</p>
-      <svg onClick={() => deleteTodo(stuff.id)} height={20} width={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+      <p onClick={() => deleteTodo(stuff.id)} className="complete">Done !</p>
     </div>
   );
 }
