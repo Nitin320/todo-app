1db2b006a50d71f7721ffb95d86d57034f790f75
Nitin <nitinkm320@gmail.com>
2024-02-04 23:00:31 +0530

    DONE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,26 @@ import Image from "next/image";
 import './index.css';
 import pic from "./pics/main-pic.png";
 import { orderBy, addDoc, collection, deleteDoc, doc, onSnapshot, query, updateDoc, Timestamp } from "firebase/firestore";
-import {db} from "./firebase.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQcJvIgidYB51hRJafqBOpbdIJlnejVwk",
+  authDomain: "todo-app-134e0.firebaseapp.com",
+  projectId: "todo-app-134e0",
+  storageBucket: "todo-app-134e0.appspot.com",
+  messagingSenderId: "677209122034",
+  appId: "1:677209122034:web:745fe73ca1fdb1630e1c6b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
 
 export default function Home() {
 
